569b0bd3b0a0ba13dfaada47f63788e79d53ddd3
eduardorochadev <chicoedumacedo@gmail.com>
2025-04-10 10:34:29 -0300

    configuração do cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors'); 
 const veiculoRoutes = require('./routes/veiculoRoutes');
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/api', veiculoRoutes);
 
